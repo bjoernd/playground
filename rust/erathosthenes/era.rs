f42db14adb78679f4816858b318b8ec842968b1d
Björn Döbel <bjoern.doebel@gmail.com>
2013-06-11 10:12:38 +0200

    shorten eratosthenes
    use std::range to implement for loops instead of manually
    allocating and incrementing indices

--- rust/erathosthenes/era.rs
+++ rust/erathosthenes/era.rs
@@ -1,20 +1,14 @@
 fn eratosthenes(max : uint) -> ~[bool]
 {
 	let mut prime: ~[bool] = vec::from_elem(max, true);
-	let mut i = 2;
 
-	while (i < max) {
-		let mut idx = i * i;
-		while (idx < max) {
-			if (prime[idx]) {
-				prime[idx] = false;
-			}
-			idx += i;
+	for uint::range(2u, max) |i| {
+		for uint::range_step(i*i, max, i as int) |idx| {
+			prime[idx] = false;
 		}
-		i += 1;
 	}
 
-	return prime;
+	prime
 }
 
 fn main()
@@ -26,13 +20,12 @@ fn main()
 	println("Sieve of Erathosthenes");
 
 	let prime = eratosthenes(MAX_NUM as uint);
-	let mut i = 0;
 
-	while (i < MAX_NUM as uint) {
+	for uint::range(0, MAX_NUM as uint) |i| {
 		if (prime[i]) {
 			print(fmt!("%? ", i));
 		}
-		i += 1;
 	}
+
 	println("");
 }
