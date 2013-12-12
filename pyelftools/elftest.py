f52a92f378130b02d78dc80e0115e4e707bf56a3
Björn Döbel <bjoern.doebel@gmail.com>
2013-12-12 11:47:16 +0100

    Allow to show symbol info
    * for functions and variables
    * -C demangle option uses c++filt to beautify
      C++ symbols

--- pyelftools/elftest.py
+++ pyelftools/elftest.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+"""
+Toy example for using PyELFTools by Eli Benderski.
+"""
+
 #pylint: disable=I0011,C0111,C0103
 
 import elftools.elf.elffile
@@ -7,9 +11,13 @@ import elftools.elf.segments
 import elftools.elf.sections
 import elftools.elf.constants
 import argparse
+import subprocess
 
 
 def argumentProcessing():
+    """
+    Command line argument processing
+    """
     parse = argparse.ArgumentParser()
     parse.add_argument("-f", "--file", required=True,
                        help="ELF file to parse")
@@ -17,16 +25,24 @@ def argumentProcessing():
                        help="Show section info")
     parse.add_argument("-e", "--segments", action="store_true",
                        help="Show (loadable) ELF segment info")
+    parse.add_argument("-y", "--symbols", action="store_true",
+                       help="Show symbols")
+    parse.add_argument("-C", "--demangle", action="store_true",
+                       help="Demangle C++ symbol names")
     return parse.parse_args()
 
 
 def banner(string):
+    """Print string as a fancy banner."""
     print
     print "___...---===### \033[36;1m%s\033[0m ###===---...___" % string
     print
 
 
 def generalELFInfo(elffile):
+    """
+    Display general header information from the ELF file
+    """
     banner("General ELF Info")
     print "=== Architecture: %s" % elffile.get_machine_arch()
     print "=== Has DWARF Info: %s" % elffile.has_dwarf_info()
@@ -38,20 +54,56 @@ def generalELFInfo(elffile):
 
 
 def segmentInfo(elffile):
+    """
+    Display information about the ELF file's loadable segments.
+    """
     banner("Loadable Segment Info")
     for idx in range(elffile.num_segments()):
         seg = elffile.get_segment(idx)
-        if seg.header["p_type"] in ("PT_LOAD", "PT_DYNAMIC"):
+        if seg.header["p_type"] in ("PT_LOAD"):
             print "VA 0x%x" % seg.header["p_vaddr"],
             print "PA 0x%x" % seg.header["p_paddr"],
             print "OF 0x%x" % seg.header["p_offset"]
-            
+
 def sectionInfo(elffile):
+    """
+    Display the ELF file's section names and types.
+    """
     banner("Section Info")
     for idx in range(elffile.num_sections()):
         sec = elffile.get_section(idx)
         print "Section %d: '%s'" % (idx, sec.name),
         print sec.header["sh_type"]
+
+def singleSymbolInfo(sym, demangle):
+    """
+    Print information about a single symbo if it is a function
+    or variable
+    """
+    st_type = sym.entry["st_info"]["type"]
+    if st_type in ("STT_FUNC", "STT_OBJECT", "STT_TLS"):
+        if demangle:
+            name = subprocess.check_output(["c++filt", sym.name]).strip()
+        else:
+            name = sym.name
+        print "\033[33m0x%08x \033[32m%50s\033[0m"  % \
+            (sym.entry["st_value"], name),
+        print "%15s" % st_type
+
+
+def symbolInfo(elffile, demangle):
+    """
+    Show symbols in ELF file
+    """
+    banner("Symbol Info")
+    for idx in range(elffile.num_sections()):
+        sec = elffile.get_section(idx)
+        # pyelftools creates a SymbolTableSection for SHT_SYMTAB and
+        # SHT_DYNSYM sections
+        if isinstance(sec, elftools.elf.sections.SymbolTableSection):
+            print "Found a symbol table in section %d (%s)" % (idx, sec.name)
+            for sym in sec.iter_symbols():
+                singleSymbolInfo(sym, demangle)
 
 def main():
     opt = argumentProcessing()
@@ -65,6 +117,8 @@ def main():
         segmentInfo(ep)
     if opt.sections:
         sectionInfo(ep)
+    if opt.symbols:
+        symbolInfo(ep, opt.demangle)
 
 if __name__ == "__main__":
     main()
