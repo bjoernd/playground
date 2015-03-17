b72e5da0fa43986f8263c20a59e5c4f9a9a879db
Björn Döbel <bjoern.doebel@gmail.com>
2015-03-17 13:54:34 +0100

    more Rust playstuff

--- rust/variables/src/main.rs
+++ rust/variables/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(old_io)]
+
 // XXX: template fn would be nice...
 fn dec_val(x : u32) -> u32 {
     println!("Decrementing");
@@ -59,6 +61,77 @@ fn structs() {
     print_foobarbaz(f);
 }
 
+use std::cmp::Ordering;
+
+fn cmp_numbers(a : i32, b : i32) -> Ordering {
+    if a < b { Ordering::Less }
+    else if a > b { Ordering::Greater }
+    else { Ordering::Equal }
+}
+
+fn matching() {
+    let foo : i32 = 15;
+    let bar : i32 = 23;
+
+    // variant A
+    match cmp_numbers(foo, bar) {
+        Ordering::Less    => println!("{} is less than {}", foo, bar),
+        Ordering::Equal   => println!("{} is equal to {}", foo, bar),
+        Ordering::Greater => println!("{} is greater than {}", foo, bar)
+    }
+
+    // variant B
+    println!("{} is {} {}", bar,
+             match cmp_numbers(bar, foo) {
+                 Ordering::Less => "less than",
+                 Ordering::Equal => "equal to",
+                 Ordering::Greater => "greather than",
+             },
+             foo);
+}
+
+
+fn looping() {
+    for x in 1..10 {
+        println!("x = {}", x);
+    }
+}
+
+
+/*
+   Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz"
+   instead of the number and for the multiples of five print "Buzz". For numbers which are
+   multiples of both three and five print "FizzBuzz".
+*/
+fn fizzbuzz() {
+    for i in 1..101 {
+        if i % 15 == 0 { println!("Fizzbuzz"); }
+        else if i % 5 == 0 { println!("Buzz"); }
+        else if i % 3 == 0 { println!("Fizz"); }
+        else { println!("{}", i); }
+    }
+}
+
+
+fn vectors() {
+    let mut v = vec!["a", "b", "c"];
+    v.push("d");
+    for c in v {
+        println!("{}", c);
+    }
+}
+
+use std::old_io;
+
+fn user_io() {
+    println!("What's your name?");
+    let name = old_io::stdin()
+                    .read_line()
+                    .ok()
+                    .expect("Failed to read a line");
+    println!("Hello {}!", name.trim());
+}
+
 fn main() {
     println!("============================================================");
     formatting();
@@ -66,5 +139,15 @@ fn main() {
     tuples();
     println!("============================================================");
     structs();
+    println!("============================================================");
+    matching();
+    println!("============================================================");
+    looping();
+    println!("============================================================");
+    fizzbuzz();
+    println!("============================================================");
+    vectors();
+    println!("============================================================");
+    user_io();
     println!("============================================================");
 }
