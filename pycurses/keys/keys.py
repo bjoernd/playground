b322a5d8c4a9d5b0ee320a892ebb72179da7e6e4
Björn Döbel <bjoern.doebel@gmail.com>
2015-03-27 23:56:32 +0100

    key input test

---
+++ pycurses/keys/keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#pylint: disable=I0011,C0102,C0103,C0111,C0301,C0326,R0913
+
+import curses
+
+def runfunc(screen):
+    logfile = open("keys.log", "w")
+    while True:
+        c = screen.getch()
+        logfile.write("KEY: %d (%c)\n" % (c, chr(c)))
+    logfile.close()
+
+curses.wrapper(runfunc)
