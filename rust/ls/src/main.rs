085b492213c0ef832b8c4748a6f93aba14f18206
Bjoern Doebel <bjoern.doebel@gmail.com>
2017-07-15 20:13:08 +0000

    Config refactoring
    * create separate struct to hold parameters
    * move config reading into separate function

--- rust/ls/src/main.rs
+++ rust/ls/src/main.rs
@@ -2,9 +2,25 @@
 extern crate clap;
 use clap::App;
 
-fn main() {
-    let yaml=load_yaml!("cli.yml");
+struct Configuration {
+    long_mode: bool, /* -l */
+}
+
+fn read_config() -> Configuration {
+    let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
-    let long_mode = matches.is_present("long");
-    println!("Long mode: {}", long_mode);
+
+    let mut config = Configuration {
+        long_mode: false,
+    };
+
+    config.long_mode = matches.is_present("long");
+
+    config
+}
+
+fn main() {
+    let conf = read_config();
+
+    println!("Long mode: {}", conf.long_mode);
 }
